032d8c6de09007a46bfd07f5ddef3253e93d7d8d
Louis Thevenet <louis.tvnt@gmail.com>
2025-03-20 10:33:51 +0100

    build(nix): improve flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
-	description = "Flake to run Deno";
-	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
 
-	outputs = { self, nixpkgs }:
-		let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-		in {
-			devShell.x86_64-linux = pkgs.mkShell {
-				buildInputs = with pkgs; [
-					deno
-				];
-			};
-		};
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [ deno ];
+          };
+
+        };
+    };
 }
