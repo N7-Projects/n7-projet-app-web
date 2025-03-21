76aeb7a57eb88ba891b5bb5fa6074123c2c3c74a
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-03-21 16:45:53 +0100

    chore: create maven project

---
+++ server/src/test/java/hagimetaceinture/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hagimetaceinture.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
