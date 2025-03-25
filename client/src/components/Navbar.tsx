950d35c3b75064a4e2930de6c3a44c8c57be1f7c
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-03-25 11:25:15 +0100

    feat: basic navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import { Button } from "primereact/button";
+import "./Navbar.scss";
+
+/* Access path to the logo of Photo7 */
+const logoFlagPath: string = "./racing-flag-2-svgrepo-com.svg";
+
+export default function Navbar() {
+  return (
+    <section className="navbar">
+      <Button
+        className="image"
+        onClick={() => {
+          globalThis.location.href = "/";
+        }}
+      >
+        <img src={logoFlagPath} alt="Race flags logo" />
+      </Button>
+
+      <ul className="navbar-desktop">
+        <li key="Circuits">
+          <Button
+            label="Circuits"
+            size="large"
+            text
+            onClick={() => {
+              globalThis.location.href = "/circuits";
+            }}
+          />
+        </li>
+        <li key="Calendrier">
+          <Button
+            label="Calendrier"
+            text
+            onClick={() => {
+              globalThis.location.href = "/calendar";
+            }}
+          />
+        </li>
+        <li key="Forum">
+          <Button
+            label="Forum"
+            text
+            onClick={() => {
+              globalThis.location.href = "/forum";
+            }}
+          />
+        </li>
+        <li key="Newsletter">
+          <Button
+            label="Newsletter"
+            outlined
+            onClick={() => {
+              globalThis.location.href = "/subscribe";
+            }}
+          />
+        </li>
+      </ul>
+    </section>
+  );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import "./Home.css";
+import Navbar from "../components/Navbar.tsx";
+import "./Home.scss";
 
 function Home() {
   const [count, setCount] = useState(0);
@@ -23,29 +24,9 @@ function Home() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src="/react.svg" className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React check</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => setCount((count: number) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="home">
+        <Navbar />
+      </section>
     </>
   );
 }
