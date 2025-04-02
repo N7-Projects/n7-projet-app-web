0ff54136d7e6dbc0e714c0709ee326eb6eb2eb99
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-04-02 16:40:19 +0200

    feat: will add form next

--- client/src/pages/NewCircuit.tsx
+++ client/src/pages/NewCircuit.tsx
@@ -1,5 +1,41 @@
+import { Button, Card } from "primereact";
+
 function NewCircuit() {
-  return <></>;
+  const header = (
+    <img
+      alt="Card"
+      src="/usercard.png"
+    />
+  );
+  const footer = (
+    <>
+      <Button
+        label="Create"
+        severity="primary"
+        icon="pi pi-send"
+        // Use Link if its not what do we want
+      />
+      <Button
+        label="Clear"
+        severity="danger"
+        icon="pi pi-eraser"
+        style={{ marginLeft: "0.5em" }}
+      />
+    </>
+  );
+
+  return (
+    <div className="circuit-new-card flex justify-content-center">
+      <Card
+        title="Create new circuit"
+        subTitle="Give the info"
+        footer={footer}
+        header={header}
+        className="w-6"
+      >
+      </Card>
+    </div>
+  );
 }
 
 export default NewCircuit;
