8853e6f618619f062459cd5ce6a676cef9fb1430
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-06-01 18:09:56 +0200

    wip: responsive navbar to width (still unsure)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,12 +8,14 @@ const logoFlagPath: string = "/racing-flags.svg";
 export default function Navbar() {
   const userAuthed = useAuth();
 
-  //   const user = userAuthed?.connected().data;
-  //   console.log("AAAAA");
-  //   console.log(user);c
-
   return (
-    <section className="navbar">
+    <section className="flex
+  justify-content-between
+  align-items-center
+  static
+  h-3rem
+  my-3
+  mx-0 ">
       <Button
         severity="secondary"
         outlined
@@ -22,162 +24,156 @@ export default function Navbar() {
           globalThis.location.href = "/";
         }}
       >
-        <img src={logoFlagPath} alt="Race flags logo" />
+        <img
+          src={logoFlagPath}
+          alt="Race flags logo"
+          style={{ width: "4rem", margin: "1%" }}
+        />
       </Button>
 
-      <ul className="navbar-desktop">
-        <li key="Circuits">
-          <SplitButton
-            label="Circuits"
-            icon="pi pi-car"
-            dropdownIcon="pi pi-caret-down"
-            outlined
-            onClick={() => {
-              globalThis.location.href = "/circuits";
-            }}
-            model={[
-              {
-                label: "Tous",
-                icon: "pi pi-bars",
-                command: () => {
-                  globalThis.location.href = "/circuits";
-                },
+      <div className="navbar-desktop">
+        <SplitButton
+          label="Circuits"
+          icon="pi pi-car"
+          dropdownIcon="pi pi-caret-down"
+          outlined
+          onClick={() => {
+            globalThis.location.href = "/circuits";
+          }}
+          model={[
+            {
+              label: "Tous",
+              icon: "pi pi-bars",
+              command: () => {
+                globalThis.location.href = "/circuits";
               },
-              {
-                label: "Nouveau",
-                icon: "pi pi-plus",
-                command: () => {
-                  globalThis.location.href = "/circuits/new";
-                },
+            },
+            {
+              label: "Nouveau",
+              icon: "pi pi-plus",
+              command: () => {
+                globalThis.location.href = "/circuits/new";
               },
-            ]}
-          />
-        </li>
+            },
+          ]}
+        />
 
-        <li key="Equipes">
-          <SplitButton
-            label="Equipes"
-            icon="pi pi-users"
-            dropdownIcon="pi pi-caret-down"
-            outlined
-            onClick={() => {
-              globalThis.location.href = "/teams";
-            }}
-            model={[
-              {
-                label: "Tous",
-                icon: "pi pi-bars",
-                command: () => {
-                  globalThis.location.href = "/teams";
-                },
+        <SplitButton
+          label="Equipes"
+          icon="pi pi-users"
+          dropdownIcon="pi pi-caret-down"
+          outlined
+          onClick={() => {
+            globalThis.location.href = "/teams";
+          }}
+          model={[
+            {
+              label: "Tous",
+              icon: "pi pi-bars",
+              command: () => {
+                globalThis.location.href = "/teams";
               },
-              {
-                label: "Nouveau",
-                icon: "pi pi-plus",
-                command: () => {
-                  globalThis.location.href = "/teams/new";
-                },
+            },
+            {
+              label: "Nouveau",
+              icon: "pi pi-plus",
+              command: () => {
+                globalThis.location.href = "/teams/new";
               },
-            ]}
-          />
-        </li>
+            },
+          ]}
+        />
 
-        <li key="Calendrier">
-          <Button
-            className="nav-button"
-            label="Calendrier"
-            icon="pi pi-calendar"
-            outlined
-            onClick={() => {
-              globalThis.location.href = "/calendar";
-            }}
-          />
-        </li>
-        <li key="Forum">
-          <Button
-            className="nav-button"
-            label="Forum"
-            icon="pi pi-comments"
-            outlined
-            onClick={() => {
-              globalThis.location.href = "/forum";
-            }}
-          />
-        </li>
-        <li key="Newsletter">
-          <Button
-            className="nav-button"
-            label="Newsletter"
-            icon="pi pi-send"
-            outlined
-            onClick={() => {
-              globalThis.location.href = "/subscribe";
-            }}
-          />
-        </li>
+        <Button
+          className="nav-button"
+          label="Calendrier"
+          icon="pi pi-calendar"
+          outlined
+          onClick={() => {
+            globalThis.location.href = "/calendar";
+          }}
+        />
+
+        <Button
+          className="nav-button"
+          label="Forum"
+          icon="pi pi-comments"
+          outlined
+          onClick={() => {
+            globalThis.location.href = "/forum";
+          }}
+        />
+
+        <Button
+          className="nav-button"
+          label="Newsletter"
+          icon="pi pi-send"
+          outlined
+          onClick={() => {
+            globalThis.location.href = "/subscribe";
+          }}
+        />
 
         {userAuthed && userAuthed.user
           ? (
-            <li key="Login">
-              <SplitButton
-                severity="info"
-                label={userAuthed.user.name}
-                icon="pi pi-user"
-                dropdownIcon="pi pi-caret-down"
-                outlined
-                onClick={() => {
-                  globalThis.location.href = "/members/" +
-                    userAuthed.user?.idMembre;
-                }}
-                model={[
-                  {
-                    label: "Profile",
-                    icon: "pi pi-user",
-                    command: () => {
-                      globalThis.location.href = "/members/" +
-                        userAuthed.user?.idMembre;
-                    },
+            <SplitButton
+              severity="info"
+              label={userAuthed.user.name}
+              icon="pi pi-user"
+              dropdownIcon="pi pi-caret-down"
+              outlined
+              onClick={() => {
+                globalThis.location.href = "/members/" +
+                  userAuthed.user?.idMembre;
+              }}
+              model={[
+                {
+                  label: "Profile",
+                  icon: "pi pi-user",
+                  command: () => {
+                    globalThis.location.href = "/members/" +
+                      userAuthed.user?.idMembre;
                   },
-                  {
-                    label: "Se déconnecter",
-                    icon: "pi pi-sign-out",
-                    command: () => {
-                      userAuthed.logOut();
-                    },
+                },
+                {
+                  label: "Se déconnecter",
+                  icon: "pi pi-sign-out",
+                  command: () => {
+                    userAuthed.logOut();
                   },
-                ]}
-              />
-            </li>
+                },
+              ]}
+            />
           )
           : (
             <>
-              <li key="Login">
-                <Button
-                  className="nav-button"
-                  severity="info"
-                  label="Se connecter"
-                  icon="pi pi-sign-in"
-                  outlined
-                  onClick={() => {
-                    globalThis.location.href = "/login";
-                  }}
-                />
-              </li>
-              <li key="Register">
-                <Button
-                  className="nav-button"
-                  severity="info"
-                  label="Créer un compte"
-                  icon="pi pi-user-plus"
-                  outlined
-                  onClick={() => {
-                    globalThis.location.href = "/register";
-                  }}
-                />
-              </li>
+              <Button
+                className="nav-button"
+                severity="info"
+                label="Se connecter"
+                icon="pi pi-sign-in"
+                outlined
+                onClick={() => {
+                  globalThis.location.href = "/login";
+                }}
+              />
+
+              <Button
+                className="nav-button"
+                severity="info"
+                label="Créer un compte"
+                icon="pi pi-user-plus"
+                outlined
+                onClick={() => {
+                  globalThis.location.href = "/register";
+                }}
+              />
             </>
           )}
-      </ul>
+      </div>
+
+      <div className="navbar-medium">
+      </div>
     </section>
   );
 }
